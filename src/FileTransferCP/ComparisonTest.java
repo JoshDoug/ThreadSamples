a44abeab2692dec2f721099a767cc2efc3f78be7
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2017-07-30 23:52:47 +0100

    Add simple timer to thread

--- src/FileTransferCP/ComparisonTest.java
+++ src/FileTransferCP/ComparisonTest.java
@@ -4,11 +4,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class ComparisonTest {
     private static volatile boolean exit = false;
 
     public static void main(String[] args) {
+        Date start = new Date();
+
         String fileStart = "/Users/jds/Learning/Java/ThreadSamples/TestTransfer/StartZip.zip";
         String fileEnd = "/Users/jds/Learning/Java/ThreadSamples/TestTransfer/EndZip.zip";
 
@@ -25,5 +29,8 @@ public class ComparisonTest {
         } catch (IOException io) {
             io.printStackTrace();
         }
+
+        Date end = new Date();
+        System.out.println(TimeUnit.MILLISECONDS.toSeconds(start.getTime() - end.getTime()));
     }
 }
